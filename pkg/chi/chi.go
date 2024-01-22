9198ca68a84808880a5b7c370a53e2cc95aea679
motortruck1221 <tuckerj0606@icloud.com>
2024-01-21 22:09:07 -0700

    So of a

--- pkg/chi/chi.go
+++ pkg/chi/chi.go
@@ -2,7 +2,7 @@ package bare_go_chi
 
 import (
     "github.com/go-chi/chi/v5"
-    "github.com/ruby-network/bare-go/internal/routes"
+    "github.com/ruby-network/bare-server-go/internal/routes"
 )
 
 func HandleBare(directory string, chiRouter *chi.Mux) *chi.Mux {
