108d544ad83868e112dbe4ab076e4dc6a69307c4
illusionTBA <illusionztba@gmail.com>
2024-02-16 17:41:23 -0500

    cookies in v3

--- internal/v3/v3.go
+++ internal/v3/v3.go
@@ -1,52 +1,88 @@
 package v3
 
-import ( 
-    "net/http"
-    //"io/ioutil"
-    "encoding/json"
-    "net/url"
-    "fmt"
-    "github.com/go-resty/resty/v2"
+import (
+	"io"
+	"net/http"
+
+	//"io/ioutil"
+	"encoding/json"
+	"fmt"
+	"net/url"
 )
 
 func Handler(method string, userAgent string, w http.ResponseWriter, r *http.Request, headers http.Header) {
-    xBareUrl := headers.Get("X-Bare-Url")
-    //detect if websocket
-    if headers.Get("Upgrade") == "websocket" {
-        fmt.Println("Websocket Detected Performing Handshake")
-        HandleWebsocket(w, r)
-    }
-    _, err := url.ParseRequestURI(xBareUrl)
-    if err != nil { 
-        fmt.Println("Invalid URL Ignoring", xBareUrl)
-        return
-    }
-    client := resty.New()
-    //client.SetHeaders(headers)
-    client.SetHeader("User-Agent", userAgent)
-    fmt.Println("Method", method)
-    response, err := client.R().
-    //create request based on method and x-bare-url 
-        SetResult(&resty.Response{}). // or SetResult(AuthSuccess{}).
-        Execute(method, xBareUrl)
-    
-    if err != nil {
-        fmt.Println(err)
-    }
+	xBareUrl := headers.Get("X-Bare-Url")
+	xBareHeaders := headers.Get("X-Bare-headers")
 
-    jsonHeaders, err := json.Marshal(response.Header())
+	// Parse original URL
+	originalURL, err := url.Parse(xBareUrl)
+	if err != nil {
+		fmt.Println("Invalid URL Ignoring", xBareUrl)
+		return
+	}
 
-    //if x-bare-pass-headers is set add it to jsonHeaders
-    if headers.Get("X-Bare-Pass-Headers") != "" {
-        fmt.Println("Passing Headers")
-        fmt.Println(headers.Get("X-Bare-Pass-Headers"))
-        fmt.Println(response.Header())
-        fmt.Println(jsonHeaders)
-        jsonHeaders = append(jsonHeaders, []byte(headers.Get("X-Bare-Pass-Headers"))...)
-    }
+	if headers.Get("Upgrade") == "websocket" {
+		fmt.Println("Websocket Detected Performing Handshake")
+		HandleWebsocket(w, r)
+	}
+	// Create a new request
+	req, err := http.NewRequest(method, originalURL.String(), r.Body)
+	if err != nil {
+		fmt.Println(originalURL.String())
+		panic(err)
+	}
 
-    w.Header().Set("X-Bare-Status", fmt.Sprintf("%d", response.StatusCode()))
-    w.Header().Set("X-Bare-Status-Text", response.Status())
-    w.Header().Set("X-Bare-Headers", string(jsonHeaders))
-    w.Write(response.Body())
+	// Set headers from the JSON string
+	var Dheaders map[string]interface{}
+	err = json.Unmarshal([]byte(xBareHeaders), &Dheaders)
+	if err != nil {
+		fmt.Println("Error parsing headers:", err)
+	}
+
+	for k, v := range Dheaders {
+		req.Header.Set(k, v.(string))
+	}
+
+	// Forward cookies from the original request
+	for _, cookie := range r.Cookies() {
+		req.AddCookie(cookie)
+	}
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	BareResponseHeaders := make(http.Header)
+
+	for key, values := range resp.Header {
+		for _, value := range values {
+			BareResponseHeaders[key] = []string{value}
+		}
+	}
+	headersJSON, err := json.Marshal(BareResponseHeaders)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Oops something went wrong")
+	}
+	if err != nil {
+		fmt.Println("Failed to parse")
+	}
+	if resp.Header.Get("X-Bare-Pass-Headers") != "" {
+		fmt.Println("Passing Headers")
+		fmt.Println(resp.Header.Get("X-Bare-Pass-Headers"))
+
+	}
+
+	w.Header().Set("X-Bare-Status", fmt.Sprintf("%d", resp.StatusCode))
+	w.Header().Set("X-Bare-Status-Text", http.StatusText(resp.StatusCode))
+	w.Header().Set("X-Bare-Headers", string(headersJSON))
+	w.WriteHeader(resp.StatusCode)
+	// Copy response body
+
+	// fmt.Println(resp.Body)
+	io.Copy(w, resp.Body)
 }
--- internal/v3/ws.go
+++ internal/v3/ws.go
@@ -1,85 +1,98 @@
 package v3
 
 import (
-    "net/http"
-    "github.com/gorilla/websocket"
-    "fmt"
-    "sync"
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
 type Handshake struct {
-    Type string `json:"type"`
-    Remote string `json:"remote"`
-    Protocols []string `json:"protocols"`
-    Headers map[string]string `json:"headers"`
-    ForwardHeaders []string `json:"forwardHeaders"`
+	Type           string            `json:"type"`
+	Remote         string            `json:"remote"`
+	Protocols      []string          `json:"protocols"`
+	Headers        map[string]string `json:"headers"`
+	ForwardHeaders []string          `json:"forwardHeaders"`
 }
 
 type Response struct {
-    Type string `json:"type"`
-    Protocol string `json:"protocol"`
-    SetCookies []string `json:"setCookies"`
+	Type       string   `json:"type"`
+	Protocol   string   `json:"protocol"`
+	SetCookies []string `json:"setCookies"`
 }
 
 func forwardMessages(source *websocket.Conn, destination *websocket.Conn, wg *sync.WaitGroup) {
-    defer wg.Done()
-    for {
-        messageType, message, err := source.ReadMessage()
-        if err != nil { 
-            fmt.Println("Error Reading Message", err)
-            break
-        }
-        err = destination.WriteMessage(messageType, message)
-        if err != nil { 
-            fmt.Println("Error Writing Message", err)
-            break
-        }
-    }
+	defer wg.Done()
+	for {
+		messageType, message, err := source.ReadMessage()
+		if err != nil {
+			fmt.Println("Error Reading Message", err)
+			break
+		}
+		err = destination.WriteMessage(messageType, message)
+		if err != nil {
+			fmt.Println("Error Writing Message", err)
+			break
+		}
+	}
 }
 
-
 func HandleWebsocket(w http.ResponseWriter, r *http.Request) {
-    // upgrade to websocket 
-    upgrader := websocket.Upgrader{
-        CheckOrigin: func(r *http.Request) bool {
-            return true
-        },
-    }
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil { panic(err) }
-    //read from the client
-    clientHandshake := Handshake{}
-    err = conn.ReadJSON(&clientHandshake)
-    clientOrigin := clientHandshake.Headers["Origin"]
-    //auto correct http:// to https://
-    if clientOrigin[:7] == "http://" {
-        clientOrigin = "https://" + clientOrigin[7:]
-    }
-    if err != nil { panic(err) }
-    dialer := websocket.Dialer{
-        Proxy: http.ProxyFromEnvironment,
-        HandshakeTimeout: 0,
-    }
-    remoteConn, _, err := dialer.Dial(
-        clientHandshake.Remote,
-        http.Header{"Origin": []string{clientOrigin}},
-    )
-    if err != nil { panic(err) }
-    fmt.Println("Connected to remote server", remoteConn.RemoteAddr())
-    fmt.Println("Sending response to client")
-    response := Response{
-        Type: "open",
-        Protocol: "",
-        SetCookies: []string{},
-    }
-    err = conn.WriteJSON(response)
-    if err != nil { panic(err) }
-    
-    //start forwarding messages
-    var wg sync.WaitGroup
-    wg.Add(2)
-    go forwardMessages(conn, remoteConn, &wg)
-    go forwardMessages(remoteConn, conn, &wg)
-    wg.Wait()
-    //defer conn.Close()
+	// upgrade to websocket
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		panic(err)
+	}
+	//read from the client
+	clientHandshake := Handshake{}
+	err = conn.ReadJSON(&clientHandshake)
+	// fmt.Println(clientHandshake.Headers)
+	if err != nil {
+		panic(err)
+	}
+	dialer := websocket.Dialer{
+		Proxy:            http.ProxyFromEnvironment,
+		HandshakeTimeout: 0,
+	}
+
+	headers := make(http.Header)
+	for key, value := range clientHandshake.Headers {
+		if key != "Upgrade" && key != "Connection" {
+			headers.Set(key, value)
+		}
+	}
+	// fmt.Println(headers)
+
+	remoteConn, _, err := dialer.Dial(
+		clientHandshake.Remote,
+		headers,
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to remote server", remoteConn.RemoteAddr())
+	fmt.Println("Sending response to client")
+	response := Response{
+		Type:       "open",
+		Protocol:   "",
+		SetCookies: []string{},
+	}
+	err = conn.WriteJSON(response)
+	if err != nil {
+		panic(err)
+	}
+
+	//start forwarding messages
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go forwardMessages(conn, remoteConn, &wg)
+	go forwardMessages(remoteConn, conn, &wg)
+	wg.Wait()
+	//defer conn.Close()
 }
